718b259b4eec13ed9e68b9b9db2ea5d5efce2e8b
Prakashpythonengineer <jp873215@gmail.com>
2023-06-13 15:49:24 +0530

    Java Program

--- Questions/Array Concepts/Find the Length/Main.java
+++ Questions/Array Concepts/Find the Length/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         System.out.println("Length of the array is: "+(arr.length));
+
+        // Use can also toString method 
+        System.out.println("Length of the array is: "+(arr.toString()));
+        // Output: [1,2,3,4,5]
     }
     
 }
